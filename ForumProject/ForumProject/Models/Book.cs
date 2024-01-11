893641b850cbde9c5352a8ceea99056d12563ca2
Mahdi <Mahdi@DESKTOP-LQ9P5HI>
2024-01-11 12:42:54 +0330

    Add project files.

---
+++ ForumProject/ForumProject/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace ForumProject.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Authors Author { get; set; }
+    }
+    public enum Authors
+    {
+        Bob =1,
+        Hohn =2,
+        Frank =3,
+        Jims =4,
+    }
+}
